ca1420be431ac18433a211157db317aa136dae14
Ccan-devoloper <ccan3429@gmail.com>
2026-08-04 08:39:30 +0200

    Lernmodule aus Mitschrift 02.07.2026: Ehegattengrundstücke und Abbruchfälle (#9)
    * Lernmodule aus Mitschrift 02.07.2026
    
    * Vertiefung F in Modulindex aufnehmen

--- src/data/module.js
+++ src/data/module.js
@@ -5,10 +5,11 @@ import vertiefungB from "./module-vertiefung-b";
 import vertiefungC from "./module-vertiefung-c";
 import vertiefungD from "./module-vertiefung-d";
 import vertiefungE from "./module-vertiefung-e";
+import vertiefungF from "./module-vertiefung-f";
 import faelle from "./modules-faelle"; // unveränderte Fallsammlung aus den Kursmitschriften
 
 /* Reihenfolge: erst Grundlagen und Vertiefung, dann die Originalfälle. */
-export const module = [...basisA, ...basisB, ...vertiefungA, ...vertiefungB, ...vertiefungC, ...vertiefungD, ...vertiefungE, ...faelle];
+export const module = [...basisA, ...basisB, ...vertiefungA, ...vertiefungB, ...vertiefungC, ...vertiefungD, ...vertiefungE, ...vertiefungF, ...faelle];
 
 export const bereiche = [
   { id: "alle", label: "Alle Module" },
