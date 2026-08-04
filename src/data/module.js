9980e5c44cbe82a6d2b3bf9a4cef2f1d186d20b8
Ccan-devoloper <ccan3429@gmail.com>
2026-08-04 10:13:24 +0200

    Lernmodule aus Mitschrift 07.07.2026
    Fälle 129–131 zu Gratisaktien, Kapitalanpassung und § 6b-Übertragung ergänzen.

--- src/data/module.js
+++ src/data/module.js
@@ -7,10 +7,11 @@ import vertiefungD from "./module-vertiefung-d";
 import vertiefungE from "./module-vertiefung-e";
 import vertiefungF from "./module-vertiefung-f";
 import vertiefungG from "./module-vertiefung-g";
+import vertiefungH from "./module-vertiefung-h";
 import faelle from "./modules-faelle"; // unveränderte Fallsammlung aus den Kursmitschriften
 
 /* Reihenfolge: erst Grundlagen und Vertiefung, dann die Originalfälle. */
-export const module = [...basisA, ...basisB, ...vertiefungA, ...vertiefungB, ...vertiefungC, ...vertiefungD, ...vertiefungE, ...vertiefungF, ...vertiefungG, ...faelle];
+export const module = [...basisA, ...basisB, ...vertiefungA, ...vertiefungB, ...vertiefungC, ...vertiefungD, ...vertiefungE, ...vertiefungF, ...vertiefungG, ...vertiefungH, ...faelle];
 
 export const bereiche = [
   { id: "alle", label: "Alle Module" },
