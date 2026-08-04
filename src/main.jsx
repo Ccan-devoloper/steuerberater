c264150c389f3257bc0f44f84727acd344cac661
Ccan-devoloper <ccan3429@gmail.com>
2026-08-05 01:41:39 +0200

    Hausaufgaben-Portal in die Anwendung einbinden

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import PruefungsschemataPortal from "./components/PruefungsschemataPortal";
+import HausaufgabenPortal from "./components/HausaufgabenPortal";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <PruefungsschemataPortal />
+    <HausaufgabenPortal />
   </React.StrictMode>,
 );
