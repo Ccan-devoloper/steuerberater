3266513abd27d6fb94f965218f147b00d1f10d1b
Ccan-devoloper <ccan3429@gmail.com>
2026-08-05 16:13:14 +0200

    Notiz vom 30.07. aus K2-Quellenstand entfernen (#27)

--- src/components/CampusShell.jsx
+++ src/components/CampusShell.jsx
@@ -2,7 +2,13 @@ import React, { useCallback, useEffect, useState } from "react";
 import App from "../App";
 import KstCampus from "./KstCampus";
 import PruefungsschemataPortal from "./PruefungsschemataPortal";
+import { kstQuellen } from "../data/kst-module";
 import { laden, sichern } from "../lib/fortschritt";
+
+/* Reiner Bilanzstoff gehört ausschließlich in Klausur 3 und erscheint daher
+   auch nicht als bloßer Quellenhinweis im KSt-Campus. */
+const kstQuellenOhneK3 = kstQuellen.filter((quelle) => quelle.title !== "Notiz 30.07.2026");
+kstQuellen.splice(0, kstQuellen.length, ...kstQuellenOhneK3);
 
 function Klausur2Activator({ onOpen }) {
   useEffect(() => {
