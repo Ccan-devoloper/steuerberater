a1a24a3a1492d8ca7a1b7df43c30ef3bca7c1446
Ccan-devoloper <ccan3429@gmail.com>
2026-08-06 09:45:32 +0200

    Hausaufgaben-Dokument an PDF-Seitenmodul anbinden

--- src/components/HausaufgabenDokument.jsx
+++ src/components/HausaufgabenDokument.jsx
@@ -1,5 +1,5 @@
 import React, { memo, useMemo } from "react";
-import { teileHausaufgabenSeiten } from "../data/hausaufgaben-volltext-teilen";
+import { teileHausaufgabenSeiten } from "../data/hausaufgaben-seiten";
 
 function HausaufgabenDokument({ text, bezeichnung, ersteSeite = null }) {
   const seiten = useMemo(
