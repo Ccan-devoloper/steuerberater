e52f189083169a9367d89c32828d290616c997e9
Ccan-devoloper <ccan3429@gmail.com>
2026-08-18 16:36:51 +0200

    content: Aufgabenstellungen Einheit 2 nachtragen

---
+++ src/data/k1-originalfall-aufgaben-einheit2-nachtrag.js
@@ -0,0 +1,18 @@
+export default {
+  214: {
+    seiten: "Einheit 2 · Teil 2 · Beispiel 7 · PDF-S. 161",
+    fragen: ["Höhe der Umsatzsteuer?"],
+  },
+  215: {
+    seiten: "Einheit 2 · Teil 2 · Beispiel 8 · PDF-S. 161–162",
+    fragen: ["Zeitpunkt und Höhe des Vorsteuerabzugs?"],
+  },
+  216: {
+    seiten: "Einheit 2 · Teil 2 · Beispiel 9 · PDF-S. 161–162",
+    fragen: ["Höhe des Vorsteuerabzugs?"],
+  },
+  217: {
+    seiten: "Einheit 2 · Teil 2 · Beispiel 10 · PDF-S. 260",
+    fragen: ["Höhe des Vorsteuerabzugs?"],
+  },
+};
