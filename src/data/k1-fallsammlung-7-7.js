90c950083482729697efd9969ec709559178a95e
Ccan-devoloper <ccan3429@gmail.com>
2026-08-19 10:07:19 +0200

    content: Fallsammlung Kunsthandel ergänzen

---
+++ src/data/k1-fallsammlung-7-7.js
@@ -0,0 +1,23 @@
+export default {
+  "id": "7.7",
+  "seminartag": 7,
+  "kategorie": "Umstrukturierung, Reise & Sonderbesteuerung",
+  "title": "Kunsthandel",
+  "aufgabeQuelle": "Aufgaben-PDF S. 28",
+  "loesungQuelle": "Lösungs-PDF S. 48",
+  "aufgabe": [
+    "Beurteilen Sie Hans Glück!",
+    "Hans Glück (HG) betreibt seit vielen Jahren in Aachen einen Kunsthandel. Er hat von Anfang an ordnungsgemäß alle Optionen nach § 25a UStG ausgeübt.",
+    "HG kaufte aufgrund seiner guten Beziehungen im Mai 27 ein Kunstwerk (Kunstgegenstand) mit dem Namen „Stillleben in einer rasanten Zeit“ von dem Künstler Anton Apfel (AA). Nach zähen Verhandlungen einigten sich beide auf einen Kaufpreis von netto 15.000 €. HG holte das Gemälde am 01.06.27 bei AA in Flensburg ab und verbrachte es zunächst in seine Ausstellungsräume in Aachen. Der Kaufpreis wurde vor Ort in bar bezahlt und ordnungsgemäß abgerechnet quittiert.",
+    "Das Werk „Stillleben in einer rasanten Zeit“ fand innerhalb weniger Zeit in Dietmar Detail (DD) einen neuen Eigentümer. DD war tatsächlich bereit den satten Kaufpreis von 30.000 € zu bezahlen. DD übernahm am 11.11.27 freudestrahlend in Aachen das Gemälde und zahlte die 30.000 € ebenfalls bar. Eine Rechnung benötigte er nicht."
+  ],
+  "loesung": [
+    "Verkauf an DD: HG hat von der Möglichkeit des § 25a Abs. 2 UStG laut Sachverhalt Gebrauch gemacht. Deshalb kommt die Differenzbesteuerung – trotz eines Einkaufs mit Umsatzsteuer (siehe unten) – nach § 25a Abs. 2 Satz 1 Nr. 2 UStG in Betracht.",
+    "Die übrigen Voraussetzungen des § 25a Abs. 1 UStG sind ebenfalls sämtlich erfüllt (HG ist Wiederverkäufer, das Gemälde wird im Gemeinschaftsgebiet an ihn geliefert, das Gemälde ist kein Edelstein oder -metall).",
+    "Es liegt eine Lieferung (§ 3 Abs. 1 UStG) in Aachen (§ 3 Abs. 6 Satz 1, 2 UStG) gegen Entgelt vor. Die Lieferung ist steuerbar (§ 1 Abs. 1 Nr. 1 S. 1 UStG) und steuer-pflichtig zu 19% (§ 25a Abs. 5 S. 1 i.V.m. § 12 Abs. 1 UStG).",
+    "Die Bemessungsgrundlage bestimmt sich nach § 25a Abs. 3 Sätze 1, 3 und 5 UStG nach der Differenz zwischen dem VKP 30.000 € und dem EKP 16.050 € (15.000 € netto zzgl. 7% Umsatzsteuer (siehe unten)), abzgl. 19% (11.722,69 €). Die Umsatzsteuer beträgt demnach 2.227,31 € und entsteht mit Ablauf des VAZ 11/27 (§ 13 Abs. 1 Nr. 1a Satz 1 UStG).",
+    "Ankauf von AA: AA erbringt eine Lieferung (§ 3 Abs. 1 UStG) in Flensburg (§ 3 Abs. 6 Satz 1, 2 UStG) gegen Entgelt. Die Lieferung ist steuerbar (§ 1 Abs. 1 Nr. 1 S. 1 UStG) und steuer-pflichtig zu 7% (§ 12 Abs. 2 Nr. 1 UStG i.V.m. Nr. 53 der Anlage 2 zum UStG). Die Bemessungsgrundlage (§ 10 Abs. 1 Satz 1 und 2 UStG) beträgt 15.000 € und die Umsatzsteuer 1.050 €.",
+    "Diese Umsatzsteuer ist jedoch nach § 25a Abs. 5 Satz 3 UStG bei HG nicht als Vorsteuer abziehbar, erhöht aber den EKP bei der Differenzbesteuerung (siehe oben)."
+  ],
+  "overlapIds": [195, 208]
+};
