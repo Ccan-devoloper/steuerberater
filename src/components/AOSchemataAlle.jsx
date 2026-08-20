1d87cc32f9912fe02c3ea3f53098fc860a0bd740
Ccan-devoloper <ccan3429@gmail.com>
2026-08-20 12:52:44 +0200

    Festsetzungsfrist-Schema aus Einheit 3 präzisieren

--- src/components/AOSchemataAlle.jsx
+++ src/components/AOSchemataAlle.jsx
@@ -3,9 +3,11 @@ import AOSchemaEinheit1, { aoSchemaIds as ao1SchemaIds } from "./AOSchemata";
 import { AO2_SCHEMATA, ao2SchemaIds } from "./AOSchemataEinheit2";
 import { AO3_SCHEMATA, ao3SchemaIds } from "./AOSchemataEinheit3";
 import AOTortenstueckSchema from "./AOTortenstueckSchema";
+import AO3FestsetzungsfristBeginn from "./AO3FestsetzungsfristBeginn";
 
 export default function AOSchema({ id }) {
   if (id === "ao-tortenstueckfehlerlehre") return <AOTortenstueckSchema />;
+  if (id === "ao3-ff-beginn") return <AO3FestsetzungsfristBeginn />;
   const Einheit3 = AO3_SCHEMATA[id];
   if (Einheit3) return <Einheit3 />;
   const Einheit2 = AO2_SCHEMATA[id];
