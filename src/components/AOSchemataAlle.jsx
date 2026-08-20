43fa35f18315c148b38484e8d40dfae2becd651e
Ccan-devoloper <ccan3429@gmail.com>
2026-08-20 12:47:19 +0200

    AO Einheit 3 Schemas registrieren

--- src/components/AOSchemataAlle.jsx
+++ src/components/AOSchemataAlle.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import AOSchemaEinheit1, { aoSchemaIds as ao1SchemaIds } from "./AOSchemata";
 import { AO2_SCHEMATA, ao2SchemaIds } from "./AOSchemataEinheit2";
+import { AO3_SCHEMATA, ao3SchemaIds } from "./AOSchemataEinheit3";
 import AOTortenstueckSchema from "./AOTortenstueckSchema";
 
 export default function AOSchema({ id }) {
   if (id === "ao-tortenstueckfehlerlehre") return <AOTortenstueckSchema />;
+  const Einheit3 = AO3_SCHEMATA[id];
+  if (Einheit3) return <Einheit3 />;
   const Einheit2 = AO2_SCHEMATA[id];
   return Einheit2 ? <Einheit2 /> : <AOSchemaEinheit1 id={id} />;
 }
 
-export const aoSchemaIds = [...ao1SchemaIds, ...ao2SchemaIds];
+export const aoSchemaIds = [...ao1SchemaIds, ...ao2SchemaIds, ...ao3SchemaIds];
