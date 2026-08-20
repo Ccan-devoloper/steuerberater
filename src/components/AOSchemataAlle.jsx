61c02d5fe2ca2ad32f24e26c5b6a1126b8a5487b
Ccan-devoloper <ccan3429@gmail.com>
2026-08-20 12:53:28 +0200

    Ablaufhemmungen Einheit 3 präzisieren

--- src/components/AOSchemataAlle.jsx
+++ src/components/AOSchemataAlle.jsx
@@ -4,10 +4,12 @@ import { AO2_SCHEMATA, ao2SchemaIds } from "./AOSchemataEinheit2";
 import { AO3_SCHEMATA, ao3SchemaIds } from "./AOSchemataEinheit3";
 import AOTortenstueckSchema from "./AOTortenstueckSchema";
 import AO3FestsetzungsfristBeginn from "./AO3FestsetzungsfristBeginn";
+import AO3AblaufhemmungenSchema from "./AO3AblaufhemmungenSchema";
 
 export default function AOSchema({ id }) {
   if (id === "ao-tortenstueckfehlerlehre") return <AOTortenstueckSchema />;
   if (id === "ao3-ff-beginn") return <AO3FestsetzungsfristBeginn />;
+  if (id === "ao3-ablaufhemmungen") return <AO3AblaufhemmungenSchema />;
   const Einheit3 = AO3_SCHEMATA[id];
   if (Einheit3) return <Einheit3 />;
   const Einheit2 = AO2_SCHEMATA[id];
