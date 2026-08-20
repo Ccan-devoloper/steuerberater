11d1d9d7cb706340fb6fda700da78e682af09512
Ccan-devoloper <ccan3429@gmail.com>
2026-08-20 14:47:13 +0200

    AO Einheit 5 Schemata registrieren

--- src/components/AOSchemataAlle.jsx
+++ src/components/AOSchemataAlle.jsx
@@ -3,6 +3,7 @@ import AOSchemaEinheit1, { aoSchemaIds as ao1SchemaIds } from "./AOSchemata";
 import { AO2_SCHEMATA, ao2SchemaIds } from "./AOSchemataEinheit2";
 import { AO3_SCHEMATA, ao3SchemaIds } from "./AOSchemataEinheit3";
 import { AO4_SCHEMATA, ao4SchemaIds } from "./AOSchemataEinheit4";
+import { AO5_SCHEMATA, ao5SchemaIds } from "./AOSchemataEinheit5";
 import AOTortenstueckSchema from "./AOTortenstueckSchema";
 import AO3FestsetzungsfristBeginn from "./AO3FestsetzungsfristBeginn";
 import AO3AblaufhemmungenSchema from "./AO3AblaufhemmungenSchema";
@@ -11,6 +12,8 @@ export default function AOSchema({ id }) {
   if (id === "ao-tortenstueckfehlerlehre") return <AOTortenstueckSchema />;
   if (id === "ao3-ff-beginn") return <AO3FestsetzungsfristBeginn />;
   if (id === "ao3-ablaufhemmungen") return <AO3AblaufhemmungenSchema />;
+  const Einheit5 = AO5_SCHEMATA[id];
+  if (Einheit5) return <Einheit5 />;
   const Einheit4 = AO4_SCHEMATA[id];
   if (Einheit4) return <Einheit4 />;
   const Einheit3 = AO3_SCHEMATA[id];
@@ -19,4 +22,4 @@ export default function AOSchema({ id }) {
   return Einheit2 ? <Einheit2 /> : <AOSchemaEinheit1 id={id} />;
 }
 
-export const aoSchemaIds = [...ao1SchemaIds, ...ao2SchemaIds, ...ao3SchemaIds, ...ao4SchemaIds];
+export const aoSchemaIds = [...ao1SchemaIds, ...ao2SchemaIds, ...ao3SchemaIds, ...ao4SchemaIds, ...ao5SchemaIds];
