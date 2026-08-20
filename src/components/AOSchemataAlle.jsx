e1198dcafabd109459a5ab71b6e7ec7a766c59a2
Ccan-devoloper <ccan3429@gmail.com>
2026-08-20 12:31:41 +0200

    Neue Tortenstück-Nachzeichnung im AO-Campus aktivieren

--- src/components/AOSchemataAlle.jsx
+++ src/components/AOSchemataAlle.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import AOSchemaEinheit1, { aoSchemaIds as ao1SchemaIds } from "./AOSchemata";
 import { AO2_SCHEMATA, ao2SchemaIds } from "./AOSchemataEinheit2";
+import AOTortenstueckSchema from "./AOTortenstueckSchema";
 
 export default function AOSchema({ id }) {
+  if (id === "ao-tortenstueckfehlerlehre") return <AOTortenstueckSchema />;
   const Einheit2 = AO2_SCHEMATA[id];
   return Einheit2 ? <Einheit2 /> : <AOSchemaEinheit1 id={id} />;
 }
