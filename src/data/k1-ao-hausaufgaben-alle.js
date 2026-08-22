df7e00581e8751a62f9980958ee7c74607060650
Ccan-devoloper <ccan3429@gmail.com>
2026-08-22 19:35:06 +0200

    feat: AO Hausaufgaben aggregieren

---
+++ src/data/k1-ao-hausaufgaben-alle.js
@@ -0,0 +1,25 @@
+import {k1AoHausaufgaben as bisherigeHausaufgaben,k1AoHausaufgabenDidaktik} from "./k1-ao-hausaufgaben.js";
+import {k1AoHausaufgabe3,AO_HAUSAUFGABE3_PAGE_PLAN} from "./k1-ao-hausaufgaben-3.js";
+
+export {k1AoHausaufgabenDidaktik,AO_HAUSAUFGABE3_PAGE_PLAN};
+
+export const k1AoHausaufgaben=[...bisherigeHausaufgaben,k1AoHausaufgabe3];
+
+export const AO_HAUSAUFGABEN_PAGE_PLANS=new Map([
+  ["AO-HA-1-2",{1:"didaktik",2:"AO-HA12-1/AO-HA12-2",3:"AO-HA12-3",4:"AO-HA12-4",5:"AO-HA12-1",6:"AO-HA12-1/AO-HA12-2",7:"AO-HA12-2/AO-HA12-3",8:"AO-HA12-3/AO-HA12-4",9:"AO-HA12-4"}],
+  ["AO-HA-3",AO_HAUSAUFGABE3_PAGE_PLAN],
+]);
+
+export const AO_HAUSAUFGABEN_BY_MODULE=(()=>{
+  const map=new Map();
+  for(const termin of k1AoHausaufgaben){
+    for(const fall of termin.faelle||[]){
+      for(const modulId of fall.querverweise||[]){
+        const liste=map.get(modulId)||[];
+        liste.push({terminId:termin.id,fachtermin:termin.fachtermin,fallId:fall.id,nummer:fall.nummer,titel:fall.titel});
+        map.set(modulId,liste);
+      }
+    }
+  }
+  return map;
+})();
