4bdb467c20e294fa0fa6ab04bf7abbe8126ec808
Ccan-devoloper <ccan3429@gmail.com>
2026-08-22 23:56:08 +0200

    AO Hausaufgabe 5: exakte PDF-Quelle verwenden

--- src/data/k1-ao-hausaufgaben-originaltexte.js
+++ src/data/k1-ao-hausaufgaben-originaltexte.js
@@ -3,8 +3,7 @@ import {ORIGINALSEITEN_3_P1} from "./k1-ao-hausaufgaben-original-3a.js";
 import {ORIGINALSEITEN_3_P2} from "./k1-ao-hausaufgaben-original-3b.js";
 import {ORIGINALSEITEN_4_P1} from "./k1-ao-hausaufgaben-original-4a.js";
 import {ORIGINALSEITEN_4_P2} from "./k1-ao-hausaufgaben-original-4b.js";
-import {ORIGINALSEITEN_5_P1} from "./k1-ao-hausaufgaben-original-5a.js";
-import {ORIGINALSEITEN_5_P2} from "./k1-ao-hausaufgaben-original-5b.js";
+import {ORIGINALSEITEN_5_EXAKT} from "./k1-ao-hausaufgaben-original-5-exakt.js";
 import {ORIGINALSEITEN_6_P1} from "./k1-ao-hausaufgaben-original-6a.js";
 import {ORIGINALSEITEN_6_P2} from "./k1-ao-hausaufgaben-original-6b.js";
 import {ORIGINALSEITEN_7_P1} from "./k1-ao-hausaufgaben-original-7a.js";
@@ -19,7 +18,7 @@ export const AO_HAUSAUFGABEN_ORIGINALSEITEN=new Map([
   ["AO-HA-1-2",ORIGINALSEITEN_1_2],
   ["AO-HA-3",[...ORIGINALSEITEN_3_P1,...ORIGINALSEITEN_3_P2]],
   ["AO-HA-4",[...ORIGINALSEITEN_4_P1,...ORIGINALSEITEN_4_P2]],
-  ["AO-HA-5",[...ORIGINALSEITEN_5_P1,...ORIGINALSEITEN_5_P2]],
+  ["AO-HA-5",ORIGINALSEITEN_5_EXAKT],
   ["AO-HA-6",[...ORIGINALSEITEN_6_P1,...ORIGINALSEITEN_6_P2]],
   ["AO-HA-7",[...ORIGINALSEITEN_7_P1,...ORIGINALSEITEN_7_P2]],
   ["AO-HA-8",[...ORIGINALSEITEN_8_P1,...ORIGINALSEITEN_8_10_13,...ORIGINALSEITEN_8_14_17,...ORIGINALSEITEN_8_18_21,...ORIGINALSEITEN_8_22_25]],
