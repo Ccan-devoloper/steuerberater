dd9b3899a6f725d797ca7cdf94af098d40455f98
Ccan-devoloper <ccan3429@gmail.com>
2026-08-22 22:00:34 +0200

    AO Hausaufgabe 4 in Sammelansicht integrieren

--- src/data/k1-ao-hausaufgaben-alle.js
+++ src/data/k1-ao-hausaufgaben-alle.js
@@ -1,13 +1,15 @@
 import {k1AoHausaufgaben as bisherigeHausaufgaben,k1AoHausaufgabenDidaktik} from "./k1-ao-hausaufgaben.js";
 import {k1AoHausaufgabe3,AO_HAUSAUFGABE3_PAGE_PLAN} from "./k1-ao-hausaufgaben-3.js";
+import {k1AoHausaufgabe4,AO_HAUSAUFGABE4_PAGE_PLAN} from "./k1-ao-hausaufgaben-4.js";
 
-export {k1AoHausaufgabenDidaktik,AO_HAUSAUFGABE3_PAGE_PLAN};
+export {k1AoHausaufgabenDidaktik,AO_HAUSAUFGABE3_PAGE_PLAN,AO_HAUSAUFGABE4_PAGE_PLAN};
 
-export const k1AoHausaufgaben=[...bisherigeHausaufgaben,k1AoHausaufgabe3];
+export const k1AoHausaufgaben=[...bisherigeHausaufgaben,k1AoHausaufgabe3,k1AoHausaufgabe4];
 
 export const AO_HAUSAUFGABEN_PAGE_PLANS=new Map([
   ["AO-HA-1-2",{1:"didaktik",2:"AO-HA12-1/AO-HA12-2",3:"AO-HA12-3",4:"AO-HA12-4",5:"AO-HA12-1",6:"AO-HA12-1/AO-HA12-2",7:"AO-HA12-2/AO-HA12-3",8:"AO-HA12-3/AO-HA12-4",9:"AO-HA12-4"}],
   ["AO-HA-3",AO_HAUSAUFGABE3_PAGE_PLAN],
+  ["AO-HA-4",AO_HAUSAUFGABE4_PAGE_PLAN],
 ]);
 
 export const AO_HAUSAUFGABEN_BY_MODULE=(()=>{
