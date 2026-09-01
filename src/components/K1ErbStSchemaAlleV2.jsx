21b8a6f4fdf94456369f00388d3b02485e63b62f
Ccan-devoloper <ccan3429@gmail.com>
2026-09-02 00:31:27 +0200

    fix(erbst): Abschnitts- und Schema-IDs robust normalisieren

--- src/components/K1ErbStSchemaAlleV2.jsx
+++ src/components/K1ErbStSchemaAlleV2.jsx
@@ -1,6 +1,6 @@
 import React from"react";
 import K1ErbStSchemaAlle,{erbstSchemaIds as altIds}from"./K1ErbStSchemaAlle";
 import K1ErbStSchemaFort,{erbst2fSchemaIds}from"./K1ErbStSchemataEinheit2Fortsetzung";
-const SET=new Set(erbst2fSchemaIds);
-export const erbstSchemaIds=[...altIds,...erbst2fSchemaIds];
-export default function K1ErbStSchemaAlleV2({id}){return SET.has(id)?<K1ErbStSchemaFort id={id}/>:<K1ErbStSchemaAlle id={id}/>}
+const SET=new Set(erbst2fSchemaIds);export const erbstSchemaIds=[...altIds,...erbst2fSchemaIds];
+const normalisiere=id=>String(id||"").replace(/^erbst2f-schema-/,"erbst2f-").replace(/^erbst2-schema-/,"erbst2-").replace(/^erbst-schema-/,"erbst-");
+export default function K1ErbStSchemaAlleV2({id}){const ziel=normalisiere(id);return SET.has(ziel)?<K1ErbStSchemaFort id={ziel}/>:<K1ErbStSchemaAlle id={ziel}/>}
