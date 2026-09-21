de8d2fe5164f1fd5879f6767be58453d702d2d73
Ccan-devoloper <ccan3429@gmail.com>
2026-09-21 13:52:51 +0200

    Use fachInfo consistently in renderer

--- social/src/render.mjs
+++ social/src/render.mjs
@@ -38,7 +38,7 @@ export function kontext(opt = {}) {
     /* ?? statt ||: Klausurtag 0 ist ein gültiger Wert (Mindset, Kopfsache)
        und darf nicht zu 3 werden - sonst erscheint ein Mindset-Beitrag in der
        Farbe und mit dem Etikett des dritten Prüfungstags. */
-    klausur: opt.klausur ?? (opt.fach ? FAECHER[opt.fach]?.klausur : 3) ?? 3,
+    klausur: opt.klausur ?? (opt.fach ? fachInfo(opt.fach)?.klausur : 3) ?? 3,
   };
 }
 
